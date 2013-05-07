1fb2642d5f7b6b9ab390f87b8b81340dd8368778
Opiron <info@opiron.com>
2013-05-07 19:13:19 -0300

    Create Libejemplo.cpp

---
+++ Libejemplo.cpp
@@ -0,0 +1,18 @@
+/*
+Libejemplo.cpp -Descripción cpp
+Creada por Nombre Autor, Fecha
+Lanzado bajo licencia---
+*/
+
+#include "arduino.h"
+#include "Libejemplo.h"
+
+Libejemplo::Libejemplo(int variable)
+{
+  pinMode(variable, OUTPUT);  //ejemplo
+  _variable = variable;
+}
+void Libejemplo::funcionejemplo()
+{
+//código
+}
